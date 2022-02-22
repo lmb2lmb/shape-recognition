024f72622e08869fecff23930520aa5baeede370
Logan Bauman <logan_bauman@brown.edu>
2022-02-22 10:56:13 -0500

    hi

---
+++ shape-generation/draw_shapes.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+from main import rectangle
+
+class draw_shapes:
+    
+
+
+    def draw_circle(image):
+        pass
+    def draw_rectangle():
+        pass
+    def draw_triangle():
+        pass
+    def draw_pentagon():
+        pass
+    
---
+++ shape-generation/textures.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+class textures:
+    
+    def white(shape):
+        return np.ones(shape)
+    
+    def random_noise(shape):
+        return np.random.rand(shape)
+
+    def square_tiling(shape):
+        pass
+    
+    def triangle_tiling():
+        
+    
